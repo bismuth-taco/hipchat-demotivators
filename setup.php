5b05211c209ac800913f51b954fe95291ba6752b
Brad Stronger <brad@swish.com>
2014-08-26 18:52:33 -0700

    [bas] minor formatting and cleanup

--- setup.php
+++ setup.php
@@ -1,5 +1,5 @@
 <html>
-<head>
+	<head>
 		<title>HipChat Demotivator Setup</title>
 		<meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" media="screen" />
@@ -7,57 +7,83 @@
 		  body {
 		  	padding:5em;
 		  }
+		  input {
+		  	width: 300px;
+		  }
+		  textarea {
+		  	width: 700px;
+		  	height: 10em;
+		  }
 		</style>
 	</head>
 	<body>
 		
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" ></script>
 		
-<?php
-$hello = "HipChat Demotivator Setup<hr>";
-echo $hello;
-?>
-
+	<h4>
+		HipChat Demotivator Setup
+	</h4>
+	<hr>
+	
 	<p>
 		This is a simple addon for HipChat that allows users to post random demotivators with "/demotivator foo".  
-		It searches the Google Image Search API for foo and posts the first image found. 
+		It searches the Google Image Search API for `foo` and posts the first image found to the room. 
 	</p>
 	
 	<p>
 		To get started you'll need to set up a webhook with HipChat.  The callback URL has been prefilled based on 
-		the current Window's URL.  If you are running this file on localhost, you probably want to set up a <a href='http://requestb.in'>requestb.in</a> or similar and paste the data into <a href='debug.php'>debug.php</a> instead.
+		the current URL for this browser window.
+		If you are running this file on localhost, you probably want to set up a <a href='http://requestb.in'>requestb.in</a> 
+		or similar and paste the data into <a href='debug.php'>debug.php</a> instead.
 	</p>
-	Token:<input id='token'><br>
-	Webhook URL:<input id='webhookurl' value='http://requestb.in/1f3ncog1'><br>
-	Room:<input id='room' value='Theswish'><br>
+	<input id='token' placeholder='Token'> Token<br>
+	<input id='webhookurl' value='http://requestb.in/1f3ncog1'> Webhook URL<br>
+	<input id='room' value='Theswish'> Room<br>
 	<button id='addwebhook' name='addwebhook'>Add Webhook</button>
 	
 	<br><br>
-	<textarea id='currenthooks'></textarea><br>
+	Get current webhooks<br>
+	<textarea id='currenthooks' readonly></textarea><br>
 	<button id='getcurrenthooks' name='getcurrenthooks'>Get current webhooks</button>
+	
+	<br><br>
+	Delete a webhook<br>
+	<input id='deleteid' placeholder='ID'> ID<br>
+	<button id='deletehook' name='deletehook'>Delete webhook</button>
+	
 	
 	<script>
 	
 		// click handlers for the buttons
-		$('#addwebhook').click(function() {
-			addwebhook();
+		$(document).ready( function() {
+			$('#addwebhook').click(function() {
+				addwebhook();
+			});
+			
+			$('#getcurrenthooks').click(function() {
+				getcurrenthooks();
+			});
+			
+			$('#deletehook').click(function() {
+				deletewebhook();
+			});
 		});
-		
-		$('#getcurrenthooks').click(function() {
-			getcurrenthooks();
-		});
-		
+			
 		// add a webhook -- https://www.hipchat.com/docs/apiv2/method/create_webhook
 		function addwebhook() {
 			var token = $('#token').val();
-			var url = $('#webhookurl').val();
+			var baseurl = $('#webhookurl').val();
 			var room = $('#room').val();
+
+			// add the token onto the webhook url so that it can post the images with proper credentials
+			// depending on whether baseurl already contains a '?', use an ampersand or question mark to append the token parameter 
+			url =  baseurl + ((baseurl.indexOf("?") > -1) ? "&" : "?" ) + "token="+token;
 
 			var param = {
 				url: url,
 				pattern: "/mot.*",
 				event: "room_message",
-				name: (Math.random() + "").substr(2)
+				name: "hcdm-" + Math.random()
 			};	
 			$.ajax({
 				type: "POST",
@@ -66,17 +92,19 @@ echo $hello;
 		  	  	data: JSON.stringify(param), // must be a JSON payload to add webhook
 		  	  	contentType:"application/json; charset=utf-8",
   				success: function (data) {
-					console.log('b');
+					getcurrenthooks();
+					alert("Webhook created successfully!");
+		  	  	},
+		  	  	error: function (data) {
+		  	  		console.log(data);
+		  	  		alert("Failed to create webhook -- see console for data dump");
 		  	  	}
 			});
-			console.log('a');
 		}
 		
 		// get all webhooks -- https://www.hipchat.com/docs/apiv2/method/get_all_webhooks
-		// TODO: actually put this data into the textarea
 		function getcurrenthooks() {
 			var token = $('#token').val();
-			var url = $('#webhookurl').val();
 			var room = $('#room').val();
 
 			// these are optional
@@ -91,10 +119,43 @@ echo $hello;
 			  	jsonp: 'callback',
 		  	  	data: param, // must be URL encoded to get webhooks
   				success: function (data) {
-					console.log('b');
+					var str = "";
+					$(data['items']).each( function() {
+						// only show HipChatDeMotivator callbacks
+						if (this['name'].slice(0, 5) == "hcdm-") {
+							line = this['id'] + " - " + this['pattern'] + " - " + this['url'] + "&#13;";
+							str += line;
+						}
+					});
+					$('#currenthooks').html(str);
+		  	  	},
+		  	  	error: function (data) {
+		  	  		console.log(data);
+		  	  		alert("Failed to get webhooks -- see console for data dump");
 		  	  	}
 			});
-			console.log('a');
+		}
+		
+		// delete webhook -- https://www.hipchat.com/docs/apiv2/method/delete_webhook
+		function deletewebhook() {
+			var token = $('#token').val();
+			var room = $('#room').val();
+			var hookid = $('#deleteid').val();
+
+			// request must be DELETE
+			$.ajax({
+				type: "DELETE",
+				url: "https://api.hipchat.com/v2/room/"+room+"/webhook/"+hookid+"?auth_token="+token,
+				// no data for a DELETE
+  				success: function (data) {
+					getcurrenthooks();
+					alert("Webhook deleted successfully!");
+		  	  	},
+		  	  	error: function (data) {
+		  	  		console.log(data);
+		  	  		alert("Failed to delete webhook -- see console for data dump");
+		  	  	}
+			});
 		}
 		
 	</script>
